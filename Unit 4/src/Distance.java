5d9ee818c33f822eaf2b8c63218ec341aa80e631
basketballz <babasheweed1@gmail.com>
2019-02-09 23:01:48 -0800

    Update Distance.java

--- Unit 4/src/Distance.java
+++ Unit 4/src/Distance.java
@@ -24,17 +24,17 @@ public class Distance
 
 	public Distance(int xOne, int yOne, int xTwo, int yTwo)
 	{
-		setCoordinates(x1,y1,x2,y2);
+		setCoordinates(xOne,yOne,xTwo,yTwo);
 		
 
 	}
 
 	public void setCoordinates(int xOne, int yOne, int xTwo, int yTwo)
 	{
-		xOne = x1;
-		yOne = y1;
-		xTwo = x2;
-		yTwo = y2;
+		x1 = xOne;
+		y1 = yOne;
+		x2 = xTwo;
+		y2 = yTwo;
 
 	}
 	
@@ -42,20 +42,32 @@ public class Distance
 
 	public String determineClosest( )
 	{
-		double distanceA = 0.0, distanceB = 0.0;
+		double distanceA = 0.0, distanceB = 0.0, xorigin = 0.0 ,yorigin= 0.0;
 		
 		String result="";
 
 
-
-
+		distanceA=Math.sqrt((x1-xorigin)*(x1-xorigin)+(y1-yorigin)*(y1-yorigin));
+		distanceB=Math.sqrt((x2-xorigin)*(x2-xorigin)+(y2-yorigin)*(y2-yorigin));
+		if (distanceA<distanceB)
+		{
+			result= "A is closer to (0,0).";
+		}
+		if(distanceA==distanceB)
+		{
+			result ="Same distance";
+		}
+		if (distanceA>distanceB)
+		{
+			result = "B is closer to (0,0)";
+		}
 
 
 		return result;
 	}
 	
-	public String toString()
-	{
-		return "";
-	}
+	//public String toString()
+	//{
+		//return String.format("%.3f", result);
+	//}
 }
