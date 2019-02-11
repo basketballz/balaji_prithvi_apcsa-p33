8d4faafbe3e4b02f3ab216f37a5f7ab62b2447be
basketballz <babasheweed1@gmail.com>
2019-02-11 13:19:50 -0800

    finished unit 5 assignments
    finsih

--- Unit 5/src/StringEqualityRunner.java
+++ Unit 5/src/StringEqualityRunner.java
@@ -8,11 +8,29 @@
 
 import static java.lang.System.*;
 
+import java.util.Scanner;
+
 public class StringEqualityRunner
 {
 	public static void main( String args[] )
 	{
-		//add test cases
+		Scanner keyboard = new Scanner(System.in);
+		StringEquality cow = new StringEquality();
+		out.println("Enter two strings ::");
+		String wordOne = keyboard.nextLine();
+		String wordTwo = keyboard.nextLine();
+		cow.setWords(wordOne, wordTwo);
+		cow.checkEquality();
+		out.println(cow.toString());
+		
 		
 	}
 }
+
+
+
+
+
+
+
+
