03f57a9695bf88fd9b0063ba74e3bdddcc7b2db5
basketballz <babasheweed1@gmail.com>
2019-02-20 21:27:14 -0800

    double stuff done
    yay

--- Unit 7/src/BiggestDouble.java
+++ Unit 7/src/BiggestDouble.java
@@ -10,24 +10,40 @@ public class BiggestDouble
 
 	public BiggestDouble()
 	{
-		this(0,0,0,0);
+		setDoubles(0,0,0,0);
 	}
 
 	public BiggestDouble(double a, double b, double c, double d)
 	{
+		setDoubles(a,b,c,d);
 	}
 
 	public void setDoubles(double a, double b, double c, double d)
 	{
+				one=a;
+				two=b;
+				three=c;
+				four=d;
+				
+			
 	}
 
 	public double getBiggest()
 	{
-		return 0.0;
+		double [] cow = {one,two,three,four,};
+		double magmaman = 0.0;
+		for (int i=0;i<cow.length-1;i++)
+		{
+			if (cow[i] == one)
+			magmaman = cow[0];
+			if (cow[i+1]>magmaman)
+			magmaman = cow[i+1];
+		}
+		return magmaman;
 	}
 
 	public String toString()
 	{
-	   return "";
+	   return "biggest = "+ getBiggest();
 	}
 }
--- Unit 7/src/DoubleRunner.java
+++ Unit 7/src/DoubleRunner.java
@@ -9,8 +9,25 @@ public class DoubleRunner
 	public static void main(String[] args)
 	{
 		BiggestDouble run = new BiggestDouble(4.5,6.7,7.8,9.0);
-		System.out.println(run);
-		System.out.println("biggest = " + run.getBiggest() + "\n");
+		System.out.println("4.5 6.7 7.8 9.0");
+		System.out.println(run+"\n");
+		run.setDoubles(14.51,6.17 ,71.8 ,1.0 );
+		System.out.println("14.51 6.17 71.8 1.0");
+		System.out.println(run+"\n");
+		run.setDoubles(41.15 ,816.7 ,17.8, 19.0 );
+		System.out.println("41.15 816.7 17.8 19.0");
+		System.out.println(run+"\n");
+		run.setDoubles(884.5, 16.7, 7.8, 9.0);
+		System.out.println("884.5 16.7 7.8 9.0");
+		System.out.println(run+"\n");
+		run.setDoubles(4.5, -456.7, 677.8, 9.0);
+		System.out.println("4.5 -456.7 677.8 9.0");
+		System.out.println(run+"\n");
+		run.setDoubles(4.5 ,16.7 ,-7.8 ,-9.0);
+		System.out.println("4.5 16.7 -7.8 -9.0");
+		System.out.println(run+"\n");
+		
+		
 
 		//add more test cases
 				
