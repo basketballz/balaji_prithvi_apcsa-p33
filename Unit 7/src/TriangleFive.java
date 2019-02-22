b67848617e4043a11d75324c143c256f05c174ca
basketballz <babasheweed1@gmail.com>
2019-02-21 22:18:33 -0800

    triangleone
    lol took forever but whatevs

--- Unit 7/src/TriangleFive.java
+++ Unit 7/src/TriangleFive.java
@@ -4,6 +4,8 @@
 
 import static java.lang.System.*;
 
+import java.util.Scanner;
+
 public class TriangleFive
 {
    private char letter;
@@ -11,23 +13,79 @@ public class TriangleFive
 
 	public TriangleFive()
 	{
+		setAmount(0);
+		setLetter(' ');
 	}
 
 	public TriangleFive(char c, int amt)
 	{
+		setAmount(amt);
+		setLetter(c);
 	}
 
 	public void setLetter(char c)
 	{
+		letter = c;
 	}
 
 	public void setAmount(int amt)
 	{
+		amount = amt;
 	}
 
 	public String toString()
-	{
-		String output="";
-		return output;
+	{	
+		
+	    String s = "";
+		char y = ' ';
+		int iffer = 0;
+		int iffer1=0;
+		
+		
+		for (int i = 1; i <= amount; i++)   //ROW
+		{	
+			
+			for (int j = amount; j >= i; j--) //COLUMN
+			{
+				//if (j ==amount)
+				//{y = (char) (letter + (char) (amount - j)); 
+				//iffer = (int)y;}
+				
+				y = (char) (letter + (char) (amount - j)); 
+				iffer = (int)y;
+				if(iffer>=65 && iffer<=90)
+				{}
+				
+						else
+							{
+								iffer = 65+iffer1;
+								y = (char)iffer; }
+				
+				
+				
+				for (int k = 1; k <= j; k++) //CHAR PRINT IN EACH ROWXCOL
+				{
+					
+					s = s + y;
+				}
+				if (y == 'A')
+				{iffer1 +=1; }
+				s = s + " ";
+				
+				
+				
+							
+				
+			}
+			s=s+"\n";
+			iffer1=0;
+			
+					
+		}	
+		
+	
+
+		
+		return s;
 	}
 }
--- Unit 7/src/TriangleFiveRunner.java
+++ Unit 7/src/TriangleFiveRunner.java
@@ -4,9 +4,48 @@
 
 import static java.lang.System.*;
 
+import java.util.Scanner;
+
 public class TriangleFiveRunner
 {
    public static void main(String args[])
    {
-	}
+	   
+	   TriangleFive test = new TriangleFive('C',4);
+	   out.println(test.toString());
+	   test.setAmount(3);
+	   test.setLetter('Z');
+	   out.println(test.toString());	
+	   test.setAmount(5);
+	   test.setLetter('A');
+	   out.println(test.toString());
+	   test.setAmount(7);
+	   test.setLetter('B');
+	   out.println(test.toString());
+	   test.setAmount(6);
+	   test.setLetter('X');
+	   out.println(test.toString());
+	   test.setAmount(8);
+	   test.setLetter('Z');
+	   out.println(test.toString());
+	   test.setAmount(3);
+	   test.setLetter('Z');
+	   out.println(test.toString());
+	  
+	  
+	   /*out.println(test.toString());	 
+	   out.println(test.toString());	
+	   out.println(test.toString());	  
+	   out.println(test.toString());	
+	   out.println(test.toString());	
+	   out.println(test.toString());	
+	   out.println(test.toString());	
+	   out.println(test.toString());*/
+
+
+
+
+   }
+   
+
 }
