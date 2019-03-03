239fa809a0604db62ab60b012ad41ff3ac3bb9a3
basketballz <babasheweed1@gmail.com>
2019-03-02 16:52:26 -0800

    Update ListAverage.java
    finish lab 3 of assignments unit 9 array lists

--- Unit 9 - Arraylists/src/ListAverage.java
+++ Unit 9 - Arraylists/src/ListAverage.java
@@ -10,6 +10,27 @@ public class ListAverage
 {
 	public static double go( List<Integer> ray)
 	{
-		return 0;
+		int smallest =ray.get(0);
+		int biggest =ray.get(0);
+		
+		if (ray.size()==1)
+			return ray.get(0);
+		
+
+		for (int i=1;i<ray.size();i++)
+		{
+			if (ray.get(i)>biggest)
+			{
+				biggest = ray.get(i);
+			}
+			if (ray.get(i)<smallest)
+			{
+				smallest = ray.get(i);
+			}
+	
+		}
+		return (biggest+smallest)/2.0;
+				
+						
 	}
 }
