cc97313de27a8e4340cced9ecba3e4a1f2915125
basketballz <babasheweed1@gmail.com>
2019-03-02 18:08:34 -0800

    arraylist
    arraylist instrutction finish

--- Unit 9 - Arraylists/src/ListGetNums.java
+++ Unit 9 - Arraylists/src/ListGetNums.java
@@ -6,13 +6,29 @@
 import java.util.List;
 import java.util.ArrayList;
 
+
 public class ListGetNums
 {
-	//method go will return an array
-	//containing the first 3 numbers
-	//greater than 11
-	public static List<Integer> go( List<Integer> ray )
+//method go will return an array
+//containing the first 3 numbers
+//greater than 11
+public static List<Integer> go( List<Integer> ray )
 	{
-		return null;
+	
+	ArrayList<Integer> aplus = new ArrayList<Integer>();
+	int i =0;
+	for ( int  num  : ray   )
+	{
+		
+	        	if (i<3 && num > 11)
+	        	{
+	        		i++;
+	        		aplus.add(num);
+	        	}
+	        	
+	}
+
+	return aplus;
 	}
 }
+	
