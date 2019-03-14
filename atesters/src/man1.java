a64da5bfc166f6725480b72dc2713edbc9c70e11
basketballz <babasheweed1@gmail.com>
2019-03-13 17:43:58 -0700

    tester
    edit

---
+++ atesters/src/man1.java
@@ -0,0 +1,11 @@
+public class man1
+{
+	public static void main( String args[] )
+	{
+		String x = "Chuck";
+		String y = new String("Chuck");
+		System.out.println(x.equals(y));
+		System.out.println( 1337 );
+
+	}
+}
