40b51b3a888f5dec5a9227c846d253a66e606ae2
basketballz <babasheweed1@gmail.com>
2019-04-25 19:40:30 -0700

    Update man1.java

--- atesters/src/man1.java
+++ atesters/src/man1.java
@@ -1,33 +1,40 @@
-/*public void sharpen(int x, int y, int w, int h){
-	  Pixel[][] pixels = this.getPixels2D();
-	  for(int row = x; row < x+h; row++){
-		  for(int col = y; col < y+w; col++){
-			  pixels[row][col].setRed(pixels[row][col].getRed()+(pixels[row][col].getRed()-pixels[row-1][col-1].getRed())/2);
-			  pixels[row][col].setBlue(pixels[row][col].getBlue()+(pixels[row][col].getBlue()-pixels[row-1][col-1].getBlue())/2);
-			  pixels[row][col].setGreen(pixels[row][col].getGreen()+(pixels[row][col].getGreen()-pixels[row-1][col-1].getGreen())/2);
-			  if(pixels[row][col].getRed()>255){
-				  pixels[row][col].setRed(255);
-			  }
-			  if(pixels[row][col].getBlue()>255){
-				  pixels[row][col].setBlue(255);
-			  }
-			  if(pixels[row][col].getGreen()>255){
-				  pixels[row][col].setGreen(255);
-			  }
-			  if(pixels[row][col].getRed()<0){
-				  pixels[row][col].setRed(0);
-			  }
-			  if(pixels[row][col].getBlue()<0){
-				  pixels[row][col].setBlue(0);
-			  }
-			  if(pixels[row][col].getGreen()<0){
-				  pixels[row][col].setGreen(0);
-			  }
-		  }
-	  }
-  }*/
-
-//sharpen code , name in constructor, 
+ 
+ public void sharpen(int x, int y, int w, int h){
+	  Pixel[][] pixels = this.getPixels2D();//tygan
+	  Pixel currentPixel = null;
+	  Pixel abovePixel = null;
+	  for (int col = x; col < x + w; col++)
+	    {
+	      for (int row = y; row < y + h; row++)
+	      {
+	    	currentPixel = pixels[row][col];
+	    	abovePixel = pixels[row - 1][col - 1];
+	    	currentPixel.setBlue(currentPixel.getBlue() + (int) (0.5 * (currentPixel.getBlue() - abovePixel.getBlue())));
+	    	currentPixel.setRed(currentPixel.getRed() +(int) (0.5 * (currentPixel.getRed() - abovePixel.getRed())));
+	    	currentPixel.setGreen(currentPixel.getGreen() + (int) (0.5 * (currentPixel.getGreen() - abovePixel.getGreen())));
+	    	if(currentPixel.getRed() > 255){
+	    		currentPixel.setRed(255);
+	    	}
+	    	if(currentPixel.getBlue() > 255){
+	    		currentPixel.setBlue(255);
+	    	}
+	    	if(currentPixel.getGreen() > 255){
+	    		currentPixel.setGreen(255);
+	    	}
+	    	if(currentPixel.getRed() < 0){
+	    		currentPixel.setRed(0);
+	    	}
+	    	if(currentPixel.getBlue() < 0){
+	    		currentPixel.setBlue(0);
+	    	}
+	    	if(currentPixel.getGreen() < 0){
+	    		currentPixel.setGreen(0);
+	    	}
+	      }
+	    } 
+ }
+ 
+//sharpen code , name in constructor and class, 
 //testSharpen(50,50,500,400); runner
 
 
@@ -42,6 +49,12 @@ redMoto.explore();
 
 
 
+ 
+ 
+ 
+ 
+ 
+ 
 
 
 //blur
@@ -91,7 +104,7 @@ redMoto.explore();
 //name in second constuctor
 
 //two codes in tester
-testBlur(160, 190, 80, 80, 10);
+testBlur(190, 160, 20, 20, 10); test it out
 
 
 
